64abea871426c305c2802041309369be629cfa1e
ethan c <e@coip.me>
2020-04-27 16:33:07 -0500

    wires basic callpath for db.Ping()

--- main.go
+++ main.go
@@ -1,12 +1,41 @@
 package main
 
 import (
+	"fmt"
 	"os"
+
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
 )
+
+//needs: username, password, host, default_schema
+const dsnform = "%s:%s@tcp(%s:3306)/%s"
+
+var dsn string = fmt.Sprintf(dsnform,
+	os.Getenv("username"),
+	os.Getenv("password"),
+	os.Getenv("host"),
+	os.Getenv("default_schema"),
+)
+
 func init() {
-	os.Stdout.Write([]byte("validating mariadb"))
+	os.Stderr.Write([]byte("validating mariadb feasability...[" + dsn + "]\n"))
 }
 
 func main() {
+	var (
+		db  *sql.DB
+		err error
+	)
+
+	if db, err = sql.Open("mysql", dsn); err != nil {
+		panic(err)
+	}
+
+	if err = db.Ping(); err != nil {
+		panic(err)
+	}
+
 	os.Exit(0)
+
 }
